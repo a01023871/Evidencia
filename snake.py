e9df7cbe5c35693beafae3638a1dc4cc80ba19e9
a01023871 <73729084+a01023871@users.noreply.github.com>
2021-05-06 11:04:27 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,4 +1,6 @@
-"""Snake, classic arcade game.
+"""
+Aranza Balleza A01023871
+Snake, classic arcade game.
 Exercises
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
